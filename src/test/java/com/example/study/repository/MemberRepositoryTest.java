2bba95219bae18c6e574a753f3a7fc4853f9908b
LeeYoonSeo <diano679@gmail.com>
2023-07-10 18:15:59 +0900

    230710 QueryDSL 진행 중 / 사전 세팅 및QClass 생성 완료

--- src/test/java/com/example/study/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/study/repository/MemberRepositoryTest.java
@@ -13,8 +13,6 @@ import org.springframework.test.annotation.Rollback;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 @Transactional
 @Rollback(false)
